340ba7b1cd4126b7ff5abcdc192c49d89e3d7c30
luckmigz <lucas.matosnegri@hotmail.com>
2023-07-23 00:03:44 -0300

    Modulo de Py O.O terminado

--- conta.py
+++ conta.py
@@ -4,32 +4,54 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
+    
         print("Const {}".format(self))
-
+        
+    # Objetos de ação/ Funções	
+    
     def deposita(self,valor):
         self.__saldo =+ valor
         
-
     def saca(self,valor): 
-        self.__saldo -= valor
-        
-
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
+            
+    def __pode_sacar(self, valor_a_sacar): 
+        valor_disp = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disp
+      
     def extrato(self):
         print("Saldo de {} R$ do titular {}".format(self.__saldo,self.__titular))
         
     def transfere(self, valor, destino):
         self.saca(valor)
-        destino.deposita(valor)
+        destino.deposita(valor)   
+        
+    # Objetos de propriedade 
     
-    def get_saldo(self):
+    @property
+    def saldo(self):
         return self.__saldo
     
-    def get_limite(self):
+    @property
+    def limite(self):
         return self.__limite
-    
-    def set_limite(self,limite):
+    @limite.setter
+    def limite(self,limite):
         self.__limite = limite
-    
-    def get_titular(self):
+        
+    @property
+    def titular(self):
         return self.__titular
     
+    @property
+    def codigo_banco(self):
+        return self.__codigo_banco
+    
+    # Metodos estaticos 
+    
+    @staticmethod
+    def codigos_banco():
+        return {"BB":"001", "Caixa": "104", "Bradesco": "273"}
