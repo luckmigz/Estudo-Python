0df9c90e26bd82a851d3465e44eaea31db70bfb7
luckmigz <lucas.matosnegri@hotmail.com>
2023-07-25 14:30:26 -0300

    Continuando O.O 2

--- Modelo.py
+++ Modelo.py
@@ -1,33 +1,50 @@
-class Filme: 
-    def __init__(self, nome, ano,duracao):
-        self.__nome = nome.title()
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
         self.ano = ano
-        self.duracao = duracao
-        self.__likes = 0
-    
-    def dar_like(self):
-        self.likes += 1
-    
+        self._likes = 0
+
     @property
     def likes(self):
-         return self.__likes
-     
+        return self._likes
+
+    def dar_likes(self):
+        self._likes += 1
+
     @property
     def nome(self):
-        return self.__nome    
-   
+        return self._nome
+
     @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
+    def nome(self, nome):
+        self._nome = nome
     
-class Serie:
+
+
+class Filme(Programa): 
+    def __init__(self, nome, ano,duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+        
+    
+   
+    
+class Serie(Programa):
     def __init__(self,nome,ano,temp):
-        self.nome = nome
-        self.ano = ano
-        self.temp = temp 
+        super().__init__(nome, ano)
+        self.temporadas = temp
 
 vingadores = Filme("vingadores - guerra infinita", 2018,160)
 print(f"Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao}")
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
 
 atlanta = Serie("Atlanta",2018,2)
-print(f"Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temp}")
+print(f"Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas}")
+atlanta.dar_likes()
+atlanta.dar_likes()
+
+
+print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
+print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
